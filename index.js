fa84bf8120720aeefadf72cd27d623e5aeb5510e
Lalo <lalo.valadezvv1@gmail.com>
2022-09-12 19:49:52 -0500

    sss

--- index.js
+++ index.js
@@ -31,61 +31,61 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //app.use(cors());
 
 
-app.get("/", (req,res) => {
+app.get("/", (req, res) => {
     console.log(req)
     res.send("Paika API v1.0")
 })
 
 app.post("/interaction", (req, res) => {
-    interactionsController.insert(req,res)
+    interactionsController.insert(req, res)
 });
 
 app.post("/votes", (req, res) => {
-    votesController.insert(req,res)
+    votesController.insert(req, res)
 });
 app.post("/multipleOptions", (req, res) => {
-    multipleOptionsController.insert(req,res)
+    multipleOptionsController.insert(req, res)
 });
 app.post("/textOnly", (req, res) => {
-    TextOnlyController.insert(req,res)
+    TextOnlyController.insert(req, res)
 });
 
-
+/*
 const helperImage = (filePath, fileName, size) => {
     return sharp(filePath)
-    .resize(size)
-    .toFile(`./public/optimize/${fileName}`)
+        .resize(size)
+        .toFile(`./public/optimize/${fileName}`)
 }
 
 const storage = multer.diskStorage({
-    destination: (req,file,cb) => {
-        cb(null,'./uploads')
+    destination: (req, file, cb) => {
+        cb(null, './uploads')
     },
-    filename:(req,file,cb) => {
+    filename: (req, file, cb) => {
         const ext = file.originalname.split('.').pop()
-        cb(null,`${Date.now()}.${ext}`);
+        cb(null, `${Date.now()}.${ext}`);
     }
 })
 
 
 
-const upload = multer({storage})
+const upload = multer({ storage })
 
-app.post("/upload",upload.single('file'),(req,res) => {
+app.post("/upload", upload.single('file'), (req, res) => {
     helperImage(req.file.path, `resize-${req.file.filename}`, 300)
-    res.send({data: `resize-${req.file.filename}`});
+    res.send({ data: `resize-${req.file.filename}` });
 })
 
 const staticRoute = path.join(__dirname, './public/optimize');
 
 app.use('/image', express.static(staticRoute));
 
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
+*/
+//const PORT = process.env || 8080
 
-const PORT = process.env || 8080
-
-const server = app.listen(PORT, () => {
+const server = app.listen(9000, () => {
     console.log("Listening on port: " + 9000);
     databaseConnection.connectionToDatabase();
 });
@@ -93,111 +93,219 @@ const server = app.listen(PORT, () => {
 
 const io = require('socket.io')(server)
 
-connections =  [];
+connections = [];
 
-io.sockets.on('connection',function(socket){
+
+
+io.sockets.on('connection', function (socket) {
     connections.push(socket);
     console.log('Connect: %s sockets are connected', connections.length);
 
-    socket.on('disconnect', function(data){
-        connections.splice(connections.indexOf(socket),1)
+    socket.on('disconnect', function (data) {
+        connections.splice(connections.indexOf(socket), 1)
         console.log('Disconnect: %s sockets are connected', connections.length);
     })
 
-    socket.on('NodeJS Server Port', function(data){
+    socket.on('NodeJS Server Port', function (data) {
         console.log(data)
-        io.sockets.emit('iOS Client Port', {msg: 'Hi iOS Client'})
+        io.sockets.emit('iOS Client Port', { msg: 'Hi iOS Client' })
     })
 
-    socket.on('join', function(idRoom) {
+    socket.on('join', function (idRoom) {
+        var totalUsers = 0;
         console.log('joining room', idRoom);
         socket.join(idRoom);
+        totalUsers++;
+        io.sockets.to(idRoom).emit('totalUser', { totalUsers: totalUsers });
     });
 
-    socket.on('start', function(data) {
-        
+    socket.on('start', function (data) {
+
         console.log(data)
-        io.sockets.to(data).emit('start', {start: 1})
+        io.sockets.to(data).emit('start', { start: 1 })
         var i = 0;
         var counter = 0
         var time = 0
         var turnIndex = 0
+        var totalPrompts = 0
         var text = ""
         var img = ""
-        var numberOfPlayers
+        var finishInteraction = false
 
 
-        
-        let query = "SELECT total_prompts FROM Interactions WHERE id = "+data;
-        databaseConnection.connection.query(query, function (err, result){
-            if(err) { 
+
+        let query = "SELECT total_prompts FROM Interactions WHERE id = " + data;
+        databaseConnection.connection.query(query, function (err, result) {
+            if (err) {
                 return -1
-                }
-                let number = JSON.stringify(result[0].total_prompts)
-                turnIndex = number
-                return number
+            }
+            let number = JSON.stringify(result[0].total_prompts)
+            totalPrompts = number
+            return number
         });
-                let queryOrder = "SELECT idPrompt, type FROM Paika.Order WHERE idInteraction = "+data+" AND turn = 1";
-                databaseConnection.connection.query(queryOrder, function (err, result){
-                    console.log("seleccionando prompt")
-                    if(err) { 
-                        return 0
-                    }
-                    io.sockets.to(data).emit('type', result[0].type);
-                    let queryPrompt = "SELECT id, text,img,time FROM "+result[0].type+" WHERE idInteraction = "+data;
-                    databaseConnection.connection.query(queryPrompt, function (err, resultP){
-                        console.log("Obtener datos del promp")
-                        if(err) { 
+        let queryOrder = "SELECT idPrompt,type FROM Paika.Order WHERE idInteraction = " + data + " AND turn = 0";
+        databaseConnection.connection.query(queryOrder, function (err, result) {
+            if (err) {
+                return 0
+            }
+
+            io.sockets.to(data).emit('type', { type: result[0].type });
+            console.log("tipo prompt")
+            console.log(result[0].type)
+            io.sockets.to(data).emit('turn', turnIndex);
+            io.sockets.to(data).emit('totalPrompts', totalPrompts);
+            switch (result[0].type) {
+                case "TextOnly":
+                    let queryPromptTextOnly = "SELECT id,text,img,time FROM " + result[0].type + " WHERE idInteraction = " + data + " AND turn = 0";
+                    databaseConnection.connection.query(queryPromptTextOnly, function (err, resultP) {
+                        if (err) {
                             return 0
                         }
-                        io.sockets.to(data).emit('text', {text: resultP[0].text})
-                        io.sockets.to(data).emit('img', resultP[0].img)
-                        counter = resultP[0].time ;
-                        io.sockets.to(data).emit('totalTimer', {totalTimer: resultP[0].time})
+                        io.sockets.to(data).emit('text', { text: resultP[0].text })
+                        io.sockets.to(data).emit('img', { img: resultP[0].img })
+                        counter = resultP[0].time;
+                        io.sockets.to(data).emit('totalTimer', { totalTimer: resultP[0].time })
                     });
+                    break;
+
+                case "Votes":
+                    let queryPromptVotes = "SELECT id,text,img,time FROM " + result[0].type + " WHERE idInteraction = " + data + " AND turn = 0";
+                    databaseConnection.connection.query(queryPromptVotes, function (err, resultP) {
+                        if (err) {
+                            return 0
+                        }
+                        io.sockets.to(data).emit('text', { text: resultP[0].text })
+                        io.sockets.to(data).emit('img', { img: resultP[0].img })
+                        counter = resultP[0].time;
+                        io.sockets.to(data).emit('totalTimer', { totalTimer: resultP[0].time })
+                    });
+                    break;
+
+                case "MultipleOptions":
+                    let queryPromptMO = "SELECT id,text,img,time,option_1,option_2,option_3,option_4,option_correct FROM MultipleOptions WHERE idInteraction = " + data + " AND turn = 0";
+                    databaseConnection.connection.query(queryPromptMO, function (err, resultP) {
+                        if (err) {
+                            return 0
+                        }
+                        io.sockets.to(data).emit('text', { text: resultP[0].text })
+                        io.sockets.to(data).emit('img', { img: resultP[0].img })
+                        counter = resultP[0].time;
+                        io.sockets.to(data).emit('totalTimer', { totalTimer: resultP[0].time })
+                        io.sockets.to(data).emit('option_1', { option: resultP[0].option_1 })
+                        io.sockets.to(data).emit('option_2', { option: resultP[0].option_2 })
+                        io.sockets.to(data).emit('option_3', { option: resultP[0].option_3 })
+                        io.sockets.to(data).emit('option_4', { option: resultP[0].option_4 })
+                        io.sockets.to(data).emit('option_correct', { option: resultP[0].option_correct })
+                    });
+                    break;
+                default:
+                // code default
+            }
+            turnIndex += 1;
+        });
+
+
+        var countdown = setInterval(function () {
+
+            counter--;
+            if (counter == -5) {
+                finishInteraction = true
+            }
+
+            console.log(counter)
+            io.sockets.to(data).emit('timer', { timer: counter + 1 })
+
+
+            if (counter == 0) {
+                if (turnIndex == totalPrompts) {
+                    finishInteraction = true
+                }
+                console.log("turno Actual vs Total: " + turnIndex + " : " + totalPrompts);
+                i++;
+                console.log("i: " + i)
+                let queryOrder = "SELECT idPrompt,type FROM Paika.Order WHERE idInteraction = " + data + " AND turn = " + turnIndex;
+                databaseConnection.connection.query(queryOrder, function (err, result) {
+                    if (err) {
+                        //finishInteraction = true
+                    }
+
+                    console.log("tipo prompt")
+                    console.log(result[0].type)
+
+                    if (result[0].type == null) {
+                        finishInteraction = true
+                    }
+                    else {
+                        io.sockets.to(data).emit('type', { type: result[0].type });
+
+                        io.sockets.to(data).emit('turn', turnIndex);
+                        io.sockets.to(data).emit('totalPrompts', totalPrompts);
+                        switch (result[0].type) {
+                            case "TextOnly":
+                                let queryPromptTextOnly = "SELECT id,text,img,time FROM TextOnly WHERE idInteraction = " + data + " AND turn = " + turnIndex;
+                                databaseConnection.connection.query(queryPromptTextOnly, function (err, resultP) {
+                                    if (err) {
+                                        return 0
+                                    }
+                                    io.sockets.to(data).emit('text', { text: resultP[0].text })
+                                    io.sockets.to(data).emit('img', resultP[0].img)
+                                    counter = resultP[0].time;
+                                    io.sockets.to(data).emit('totalTimer', { totalTimer: resultP[0].time })
+                                    turnIndex += 1;
+                                });
+                                break;
+
+                            case "Votes":
+                                let queryPromptVotes = "SELECT id,text,img,time FROM Votes WHERE idInteraction = " + data + " AND turn = " + turnIndex;
+                                databaseConnection.connection.query(queryPromptVotes, function (err, resultP) {
+                                    if (err) {
+                                        return 0
+                                    }
+                                    io.sockets.to(data).emit('text', { text: resultP[0].text })
+                                    io.sockets.to(data).emit('img', resultP[0].img)
+                                    counter = resultP[0].time;
+                                    io.sockets.to(data).emit('totalTimer', { totalTimer: resultP[0].time })
+                                    turnIndex += 1;
+                                });
+                                break;
+
+                            case "MultipleOptions":
+                                let queryPromptMO = "SELECT id,text,img,time,option_1,option_2,option_3,option_4,option_correct FROM MultipleOptions WHERE idInteraction = " + data + " AND turn = " + turnIndex;
+                                databaseConnection.connection.query(queryPromptMO, function (err, resultP) {
+                                    if (err) {
+                                        return 0
+                                    }
+                                    io.sockets.to(data).emit('text', { text: resultP[0].text })
+                                    io.sockets.to(data).emit('img', resultP[0].img)
+                                    counter = resultP[0].time;
+                                    io.sockets.to(data).emit('option_1', { option: resultP[0].option_1 })
+                                    io.sockets.to(data).emit('option_2', { option: resultP[0].option_2 })
+                                    io.sockets.to(data).emit('option_3', { option: resultP[0].option_3 })
+                                    io.sockets.to(data).emit('option_4', { option: resultP[0].option_4 })
+                                    io.sockets.to(data).emit('option_correct', { option: resultP[0].option_correct })
+                                    turnIndex += 1;
+                                });
+                                break;
+                            default:
+                            // code default
+                        }
+                    }
+
+                    
+                    
+
+
                 });
 
-       
-        var countdown = setInterval(function(){
-            
-            
-            counter--
-            console.log("***")
-            console.log(counter)
-            console.log("***")
-            io.sockets.to(data).emit('timer', {timer: counter+1})
-            
-            if(counter ===0){
-                let queryOrder = "SELECT idPrompt, type FROM Paika.Order WHERE idInteraction = "+data+" AND turn = 1";
-                databaseConnection.connection.query(queryOrder, function (err, result){
-                    console.log("seleccionando prompt")
-                    if(err) { 
-                        return -1
-                    }
-                    io.sockets.to(data).emit('type', {type: result[0].type});
-                    let queryPrompt = "SELECT id, text,img,time FROM "+result[0].type+" WHERE idInteraction = "+data;
-                    databaseConnection.connection.query(queryPrompt, function (err, resultP){
-                        console.log("Obtener datos del promp")
-                        if(err) { 
-                            return -1
-                        }
-                        console.log("Textoo" + resultP[0].text)
-                        io.sockets.to(data).emit('text', {text: resultP[0].text})
-                        io.sockets.to(data).emit('img', resultP[0].img)
-                        counter = resultP[0].time;
-                        io.sockets.to(data).emit('totalTimer', {totalTimer: resultP[0].time})
-                    });
-                });
-                console.log("turno "+turnIndex)
-                console.log("vuelta: "+i)
-                    i++
-                if (turnIndex < i){
+                if (finishInteraction) {
                     clearInterval(countdown)
-                    io.sockets.to(data).emit('finish', {finish: 1})
-                    io.sockets.to(data).emit('text', {text: "Winner is "})
+                    io.sockets.to(data).emit('finish', { finish: 1 })
+                    io.sockets.to(data).emit('text', { text: "Winner is " })
+                    console.log("Finish")
                 }
+
             }
-        },1000);
+        }, 1000);
         console.log("start")
     });
 });
